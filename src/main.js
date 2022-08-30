dee8996ad1ffcc5124e29a756b2b94047160aa80
pikyr <pikytdv@gmail.com>
2022-08-30 17:41:04 -0300

    Fin de clase 07 - Maquetación del proyecto

--- src/main.js
+++ src/main.js
@@ -1,39 +1,37 @@
-const API_KEY = 'live_FOeHChUkCQhNu2wqKiUbnXSbKIX7SmYtymORUh6GCc4M5rfbagsdUxLmK0KZJ08g';
+const API_KEY =
+  "live_FOeHChUkCQhNu2wqKiUbnXSbKIX7SmYtymORUh6GCc4M5rfbagsdUxLmK0KZJ08g";
 const API = [
-  'https://api.thecatapi.com/v1',
-  '/images/search',
-  '?limit=4',
-  `&api_key=${API_KEY}`
-].join('');
+  "https://api.thecatapi.com/v1",
+  "/images/search",
+  "?limit=4",
+  `&api_key=${API_KEY}`,
+].join("");
 // console.log(API);
 
-const img1 = document.querySelector('.img1');
-const img2 = document.querySelector('.img2');
-const img3 = document.querySelector('.img3');
-const img4 = document.querySelector('.img4');
-const btn = document.querySelector('button');
+const imageContainers = document.querySelectorAll(".card__image--main");
 
-btn.addEventListener('click', nuevoGato);
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", nuevoGato);
 
 async function fetchData(urlAPI) {
   try {
     const response = await fetch(urlAPI);
     const data = await response.json();
-    
+
     return data;
-  } catch(err) {
+  } catch (err) {
     console.log("ERROR: " + err);
   }
 }
 
 async function nuevoGato() {
   const images = await fetchData(API);
-  const imageURL = images.map(image => image.url);
-  
-  img1.src = imageURL[0];
-  img2.src = imageURL[1];
-  img3.src = imageURL[2];
-  img4.src = imageURL[3];
+  const imageURL = images.map((image) => image.url);
+
+  imageContainers.forEach((container, i) => {
+    container.src = imageURL[i];
+  });
 }
 
 nuevoGato();
