deec972bf10bf012d07c97150541d498ae37f457
pikyr <pikytdv@gmail.com>
2022-08-29 21:00:26 -0300

    Fin de clase 04 - Endpoints y query parameters

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 const API = 'https://api.thecatapi.com/v1/images/search';
-const img = document.querySelector('img');
+const img1 = document.querySelector('.img1');
+const img2 = document.querySelector('.img2');
+const img3 = document.querySelector('.img3');
+const img4 = document.querySelector('.img4');
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', nuevoGato);
@@ -8,14 +11,21 @@ async function fetchData(urlAPI) {
   try {
     const response = await fetch(urlAPI);
     const data = await response.json();
-    const dataURL = data[0].url;
-  
-    return img.src = dataURL;
+    
+    return data;
   } catch(err) {
     console.log("ERROR: " + err);
   }
 }
 
-function nuevoGato() {
-  fetchData(API);
+async function nuevoGato() {
+  const images = await fetchData(`${API}?limit=4`);
+  const imageURL = images.map(image => image.url);
+  
+  img1.src = imageURL[0];
+  img2.src = imageURL[1];
+  img3.src = imageURL[2];
+  img4.src = imageURL[3];
 }
+
+nuevoGato();
