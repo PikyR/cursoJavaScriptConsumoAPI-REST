4e95028fd4a6c87b390bfc11f1649a45d85d9e5e
pikyr <pikytdv@gmail.com>
2022-08-31 17:34:19 -0300

    Fin de clase 09 - GET: /favourites

--- src/main.js
+++ src/main.js
@@ -1,37 +1,60 @@
 const API_KEY =
   "live_FOeHChUkCQhNu2wqKiUbnXSbKIX7SmYtymORUh6GCc4M5rfbagsdUxLmK0KZJ08g";
-const API = [
-  "https://api.thecatapi.com/v1",
+//
+const BASE_API_URL = "https://api.thecatapi.com/v1";
+
+const API_RANDOM = [
+  BASE_API_URL,
   "/images/search",
   "?limit=4",
   `&api_key=${API_KEY}`,
 ].join("");
-// console.log(API);
 
-const imageContainers = document.querySelectorAll(".card__image--main");
+const API_FAVOURITES = [
+  BASE_API_URL,
+  "/favourites",
+  // `?api_key=${API_KEY}`,
+].join("");
+// console.log(API_FAVOURITES);
+
+const imagesContainers = document.querySelectorAll(".card__image--main");
+const spanError = document.querySelector(".spanError");
 
 const btn = document.querySelector("button");
 
-btn.addEventListener("click", nuevoGato);
+btn.addEventListener("click", loadRandomCats);
 
 async function fetchData(urlAPI) {
   try {
     const response = await fetch(urlAPI);
     const data = await response.json();
 
+    if (response.status !== 200) {
+      spanError.inner = `Error: ${response.status}`;
+    }
+
     return data;
   } catch (err) {
-    console.log("ERROR: " + err);
+    console.log("Error name: " + err.name);
+    console.log("Error mesage: " + err.message);    
+    return spanError.innerText = `Algo a salido mal, inténtalo más tarde...`;
   }
 }
 
-async function nuevoGato() {
-  const images = await fetchData(API);
-  const imageURL = images.map((image) => image.url);
+async function loadRandomCats() {
+  const images = await fetchData(API_RANDOM);
+  const imagesURL = images.map((image) => image.url);
 
-  imageContainers.forEach((container, i) => {
-    container.src = imageURL[i];
+  imagesContainers.forEach((container, i) => {
+    container.src = imagesURL[i];
   });
 }
 
-nuevoGato();
+async function loadFavourites() {
+  const favourites = await fetchData(API_FAVOURITES);
+  
+  // console.log('favoritos', favourites.length);
+}
+
+loadRandomCats();
+loadFavourites();
